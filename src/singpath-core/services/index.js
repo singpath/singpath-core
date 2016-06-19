ddbd278b44e5af4badf04d6fbd923f12c5de9e9e
Damien Lebrun <dinoboff@hotmail.com>
2016-06-19 18:43:07 +0100

    Fix route service

--- src/singpath-core/services/index.js
+++ src/singpath-core/services/index.js
@@ -5,7 +5,7 @@ import {spfShared} from '../module.js';
 import './countries.js';
 import {config as configIcons, run as cacheIcon} from './icons/icons.js';
 import * as crypto from './crypto.js';
-import * as routes from './crypto.js';
+import * as routes from './routes.js';
 
 // TODO: refactor
 import './datastore.js';
@@ -18,7 +18,7 @@ spfShared.service('spfCrypto', crypto.Service);
 
 spfShared.constant('routes', routes.defaults);
 spfShared.factory('urlFor', routes.urlForFactory);
-spfShared.filter('urlFor', routes.urlForFilter);
+spfShared.filter('urlFor', routes.urlForFilterFactory);
 
 spfShared.config(configIcons);
 spfShared.run(cacheIcon);
